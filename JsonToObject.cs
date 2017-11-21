6073d0fa63403cba274c3ec8b2b4b148c6b835a4
Naveen Kumar <metanaveen@outlook.com>
2017-11-21 16:04:40 +0530

    Json string to object from response

--- JsonToObject.cs
+++ JsonToObject.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,20 +9,31 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace StackOverflow {
-   class JsonToObject {
-      static void MainJsonToObject (string[] args) {
 
-         //string QUrl = "https://api.pinterest.com/v1/urls/count.json?callback=receiveCount&url=" + url;
-         //System.Net.HttpWebRequest Request = (System.Net.HttpWebRequest)System.Net.WebRequest.Create (QUrl);
-         //Request.ContentType = "text/json";
-         //Request.Timeout = 10000;
-         //Request.Method = "GET";
-         //string content;
-         //using (WebResponse myResponse = Request.GetResponse ()) {
-         //   using (System.IO.StreamReader sr = new System.IO.StreamReader (myResponse.GetResponseStream (), System.Text.Encoding.UTF8)) {
-         //      content = sr.ReadToEnd ();
-         //   }
-         //};
+   #region References
+
+   // https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/JSON
+
+   #endregion References
+
+   class JsonToObject {
+      static void Main (string[] args) {
+
+         string url = "http://ticlotel.com/layered-medium-curly-hairstyles-2017.html";
+         string QUrl = "https://api.pinterest.com/v1/urls/count.json?callback=receiveCount&url=" + url;
+         System.Net.HttpWebRequest Request = (System.Net.HttpWebRequest)System.Net.WebRequest.Create (QUrl);
+         Request.ContentType = "application/json";
+         Request.Timeout = 10000;
+         Request.Method = "GET";
+         string content;
+         using (WebResponse myResponse = Request.GetResponse ()) {
+            using (System.IO.StreamReader sr = new System.IO.StreamReader (myResponse.GetResponseStream (), System.Text.Encoding.UTF8)) {
+               content = sr.ReadToEnd ();
+            }
+         };
+
+         content = content.Replace ("receiveCount(", "");
+         content = content.Remove (content.Length - 1);
 
          // receiveCount ({\"url\":\"http://www.google.com\",\"count\":75108})
 
@@ -56,7 +68,7 @@ namespace StackOverflow {
 
          #endregion RoughTry
 
-         string content = "{\"url\":\"http://www.google.com\",\"count\":75108}";
+         //string content = "{\"url\":\"http://www.google.com\",\"count\":75108}";
 
          var json = JObject.Parse (content);
          var share_url = json["url"];
